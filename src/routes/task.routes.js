c9a80db22d95f698773a215155aba56dabfe93c5
Ruslan Chepkasov <chepkasov.itrum@gmail.com>
2023-01-12 16:50:40 +0300

    validation

--- src/routes/task.routes.js
+++ src/routes/task.routes.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { validationText } = require('../middlewares/task.validation');
 
 const router = express.Router();
 
@@ -14,8 +15,8 @@ const {
 
 router.get('/tasks/:id', getTask);
 router.get('/tasks', getTasks);
-router.post('/tasks', createOneTask);
-router.patch('/tasks/text/:id', patchTaskText);
+router.post('/tasks', validationText, createOneTask);
+router.patch('/tasks/text/:id', validationText, patchTaskText);
 router.patch('/tasks/completed/:id', patchTaskCompleted);
 router.delete('/tasks/:id', deleteTask);
 router.delete('/tasks', deleteTasks);
